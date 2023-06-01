dcd4df21302773519c78afba49bc17bbb6ce9812
Otávio <otavio@cargon.com.br>
2023-06-01 19:40:50 -0300

    Dockerfile for production.

---
+++ section_6/frontend/test.js
@@ -0,0 +1,16 @@
+// test.js
+
+// Test suite
+describe("Math operations", () => {
+    // Test case 1
+    test("Addition", () => {
+      const result = 2 + 2;
+      expect(result).toBe(4);
+    });
+  
+    // Test case 2
+    test("Multiplication", () => {
+      const result = 2 * 3;
+      expect(result).toBe(6);
+    });
+  });
